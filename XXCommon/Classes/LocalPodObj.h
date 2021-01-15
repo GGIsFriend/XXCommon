088638deb583c3a4b7fd1002175b3ccb65984bac
GGIsFriend <chenhaoshangban@gmail.com>
2021-01-16 00:38:48 +0800

    add LocalPodObj

---
+++ XXCommon/Classes/LocalPodObj.h
@@ -0,0 +1,16 @@
+//
+//  LocalPodObj.h
+//  cocoapodstest
+//
+//  Created by 陈浩 on 2021/1/11.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocalPodObj : NSObject
+@property (nonatomic,copy)NSString *localStr;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XXCommon/Classes/LocalPodObj.m
@@ -0,0 +1,16 @@
+//
+//  LocalPodObj.m
+//  cocoapodstest
+//
+//  Created by 陈浩 on 2021/1/11.
+//
+
+#import "LocalPodObj.h"
+
+@implementation LocalPodObj
+
+- (NSString *)localStr
+{
+    return @"123";
+}
+@end
