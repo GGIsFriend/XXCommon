47f5e6db016e4127b9d08fa2edc2dfb4ce663394
DDIsFriend <chenhao@ddisfriend.lan>
2021-01-15 15:40:25 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-XXCommon_Example/Pods-XXCommon_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XXCommon_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XXCommon_Example
+@end
---
+++ Example/Pods/Target Support Files/XXCommon/XXCommon-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XXCommon : NSObject
+@end
+@implementation PodsDummy_XXCommon
+@end
